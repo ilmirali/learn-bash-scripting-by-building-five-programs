36df3c1d073ca2984d97f729dbda16627262387a
ilmirali <ilmirali@protonmail.com>
2022-07-31 10:41:01 +0000

    Save progress: 1410.1

--- fortune.sh
+++ fortune.sh
@@ -8,7 +8,10 @@ RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again late
 N=$(( RANDOM % 6 ))
 
 GET_FORTUNE() {
-  echo Ask a yes or no question:
+  if [[ ! $1 ]]
+  then 
+    echo Ask a yes or no question:
+  fi
   read QUESTION
 }
 
